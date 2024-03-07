5709e04981729e65b0dbf213b97bb4d7f34d8b46
Shanali Rajakaruna <it22095558@my.sliit.lk>
2024-03-07 15:46:42 +0530

    test

--- Backend/Shanali_IT22095558_Backend/server.js
+++ Backend/Shanali_IT22095558_Backend/server.js
@@ -1,1 +1,23 @@
-//erewrw
+require('dotenv').config()
+
+const express=require('express')
+
+//express app
+const app=express()
+
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+//routes
+app.get('/', (req,res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port',process.env.PORT)
+})
+
+process.env
